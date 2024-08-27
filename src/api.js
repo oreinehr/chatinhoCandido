76714798679d415227eda3300e36c86073ffec3e
reinehrgui <guilherme-reinehr@educar.rs.gov.br>
2024-08-27 18:31:53 -0300

    bomdia

---
+++ src/api.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const router = express.Router();
+
+app.use('/', router.get('/', (req, res, next) => {
+    res.status(200).send("<h1>API-CHAT</h1>");
+}));
+
+app.use('/', router.get('/sobre', (req, res, next) => {
+    res.status(200).send ({
+        "nome" : "CHATINFO",
+        "autor" : "Guilherme Reinehr",
+        "versao" : "0.1.0"
+    });
+}));
+
+app.use("/entrar", router.post("/entrar", async(req, res, next) => {
+    const usuarioController = require("./controllers/usuarioController.js");
+    let resp = await usuarioController.entrar(req.body.nick);
+    res.status(200).send(resp);
+}));
+
+app.use("/salas",router.get("/salas", async (req, res,next) => {
+    const token = require("./util/token");
+    const salaController = require("./controllers/salaController");
+    const test = await token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick);
+    console.log(test)
+    if (test) {
+        let resp = await salaController.get();
+        res.status(200).send(resp);
+    } else {
+        res.status(401).send({msg:"Usuário não autorizado"});
+    }
+})); 
+
+app.use("/sala/entrar", router.post("/sala/entrar", async (req, res) => {
+    const token = require("./util/token");
+    const salaController = require("./controllers/salaController");
+    if (token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)){
+        let resp = await salaController.entrar(req.headers.iduser, req.query.idSala);
+        res.status(200).send(resp);
+    } else{
+        res.status(401).send({msg:"Usuário não autorizado"});
+    }
+}));
+  
+app.use("/sala/enviar", router.post("/sala/enviar", async (req, res) => {
+    const token = require("./util/token");
+    const salaController = require("./controllers/salaController");
+    if (!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp = await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idSala);
+    res.status(200).send(resp);
+}))
+  
+app.use("/sala/listar", router.get("/sala/listar", async (req, res) => {
+    const token = require("./util/token");
+    const salaController = require("./controllers/salaController");
+    if (!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp = await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
+    res.status(200).send(resp);
+}))
+
+module.exports = app;
---
+++ src/models/db.js
@@ -0,0 +1,44 @@
+const { MongoClient, ObjectId } = require("mongodb");
+require('dotenv').config();
+
+let singleton;
+
+async function connect (){
+    if (singleton) return singleton; 
+
+    console.log(process.env.DB_HOST)
+    const client = new MongoClient(process.env.DB_HOST);
+    await client.connect();
+
+    singleton = client.db(process.env.DB_DATABASE);
+    return singleton;
+};
+
+let findAll = async (collection) => {
+    const db = await connect();
+    return await db.collection(collection).find().toArray();
+};  
+
+async function insertOne(collection, objeto){
+    const db = await connect();
+    return db.collection(collection).insertOne(objeto);
+};
+
+let findOne = async (collection, _id) => {
+    const db = await connect();
+    let obj= await db.collection(collection).find({'_id':new ObjectId(_id)}).toArray();
+    
+    if(obj) {
+      return obj[0];
+    };
+
+    return false;
+};
+    
+let updateOne = async (collection, object, param) => {
+    const db = await connect();
+    let result = await db.collection(collection).updateOne(param, { $set: object} );
+    return result;
+};
+
+module.exports = {findAll, insertOne, findOne, updateOne};
