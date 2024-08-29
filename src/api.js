c9d9e9f75c909a06d3ffb9fca5542260a4e75b01
reinehrgui <guilherme-reinehr@educar.rs.gov.br>
2024-08-28 21:41:07 -0300

    bomdia

--- src/api.js
+++ src/api.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');  
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 const router = express.Router();
 
-app.use('/', router.get('/', (req, res, next) => {
-    res.status(200).send("<h1>API-CHAT</h1>");
+app.use('/', router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 }));
+
 
 app.use('/', router.get('/sobre', (req, res, next) => {
     res.status(200).send ({
